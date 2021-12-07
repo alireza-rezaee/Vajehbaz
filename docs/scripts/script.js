780907b25b6c8037239ea387333f4a511ea2dcb9
Ayub Kokabi <44271870+kokabi1365@users.noreply.github.com>
2021-12-07 16:02:50 +0330

    Ability to sort donators by date or amount in the website

--- docs/scripts/script.js
+++ docs/scripts/script.js
@@ -1,53 +1,56 @@
 //This code inspired by https://github.com/rastikerdar/vazir-font/blob/gh-pages/index.html
-function loadDonators(donators) {
-  $.getJSON("https://api.github.com/repos/kokabi1365/vajehdan").done(function (
-    repo
-  ) {
-    $(".section-donation__project-life-span").html(
-      toPersianNumber(yearsSince(repo.created_at))
-    );
-  });
+var donators = [];
 
-  $(".section-donation__counter").html(toPersianNumber(donators.length));
+async function setDownloadLink() {
+  const downloadLink = (await $.getJSON('https://api.github.com/repos/kokabi1365/vajehdan/releases'))[0].assets[0].browser_download_url;
+  $('.section-header__btn').attr('href', downloadLink);
+}
 
-  $(".section-donation__amount").html(
-    toPersianNumber(
-      formatNumber(
-        donators
-          .map((donator) => donator.amount)
-          .reduce((sum, amount) => sum + amount)
-      )
-    )
-  );
+async function loadDonators() {
+  donators = await $.getJSON('scripts/donators.json');
+  await setProjectLifeTimeAsync();
+  await setTotalDonatorsCount();
+  await setTotalDonationAmount();
+  await createDonatorNodes('amount');
+}
 
+async function setProjectLifeTimeAsync() {
+  const repo = await $.getJSON('https://api.github.com/repos/kokabi1365/vajehdan');
+  $('.section-donation__project-life-span').html(toPersianNumber(yearsSince(repo.created_at)));
+}
+
+function setTotalDonatorsCount() {
+  $('.section-donation__counter').html(toPersianNumber(donators.length));
+}
+
+function setTotalDonationAmount() {
+  $('.section-donation__amount').html(toPersianNumber(formatNumber(donators.map((donator) => donator.amount).reduce((sum, amount) => sum + amount))));
+}
+
+async function createDonatorNodes(sortKey) {
+  sortBy(sortKey);
+  $('.section-donation__donators').empty();
   for (const donator of donators) {
-    donator.photo = donator.photo
-      ? donator.photo
-      : `https://eu.ui-avatars.com/api/?format=svg&background=random&name=${donator.name}`;
+    
+    donator.photo = donator.photo ? donator.photo : `https://eu.ui-avatars.com/api/?format=svg&background=random&name=${donator.name}`;
 
-    donator.job = donator.job ? donator.job : "";
-
-    var li = "";
+    var li = '';
 
     li = `<li class="donator">`;
 
-    if (donator.website)
-      li += `<a href="${donator.website}" rel="nofollow" target="_blank" title="${donator.date}">`;
-
+    if (donator.website) li += `<a href="${donator.website}" rel="nofollow" target="_blank" title="${donator.date}">`;
+    
     li += `<img class="donator__photo" src="${donator.photo}" alt="${donator.name}" title=${donator.date} loading="lazy">
-    <p class="donator__name">${donator.name}</p>`;
+      <p class="donator__name" ${donator.website?'style=color:blue;':''}>${donator.name}</p>`;
+    
 
-    if (donator.job) li += `<span class="donator__job">${donator.job}</span>`;
+    li += `<span class="donator__amount">${toPersianNumber(formatNumber(donator.amount))} تومان</span>`;
 
-    li += `<span class="donator__amount">${toPersianNumber(
-      formatNumber(donator.amount)
-    )} تومان</span>`;
+    if (donator.website) li += '</a>';
 
-    if (donator.website) li += "</a>";
+    li += '</li>\n';
 
-    li += "</li>\n";
-
-    $(".section-donation__donators").append(li);
+    $(li).appendTo('.section-donation__donators').hide().fadeIn();
   }
 }
 
@@ -58,27 +61,35 @@ function yearsSince(date) {
 }
 
 function toPersianNumber(n) {
-  const farsiDigits = ["۰", "۱", "۲", "۳", "۴", "۵", "۶", "۷", "۸", "۹"];
+  const farsiDigits = ['۰', '۱', '۲', '۳', '۴', '۵', '۶', '۷', '۸', '۹'];
   return n.toString().replace(/\d/g, (x) => farsiDigits[x]);
 }
 
 function formatNumber(x) {
-  return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, "٬");
+  return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, '٬');
 }
 
-$(function () {
-  $.getJSON("https://api.github.com/repos/kokabi1365/vajehdan/releases").done(
-    function (releases) {
-      var lastRelease = releases[0];
-      var downloadURL = lastRelease.assets[0].browser_download_url;
-      $(".section-header__btn").attr("href", downloadURL);
-    }
-  );
-
-  $.getJSON("scripts/donators.json").done(function (donators) {
+function sortBy(key) {
+  if (key == 'date') {
     donators.sort(function (a, b) {
       return b.date.localeCompare(a.date);
     });
-    loadDonators(donators);
-  });
+  } else if (key == 'amount') {
+    donators.sort(function (a, b) {
+      return b.amount - a.amount;
+    });
+  }
+
+  if (key == 'date') {
+    $('.section-donation__date_link').css({"color":"blue", "font-weight":"bold"});
+    $('.section-donation__amount_link').removeAttr("style");
+  } else {
+    $('.section-donation__date_link').removeAttr("style");
+    $('.section-donation__amount_link').css({"color":"blue", "font-weight":"bold"});
+  }
+}
+
+$(async function () {
+  await setDownloadLink();
+  await loadDonators();
 });
