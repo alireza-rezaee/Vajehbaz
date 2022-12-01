9345ea9ea3311db89b132fa2d344a6076807b499
Alireza Rezaee <alireza@rezaee.org>
2022-12-01 14:00:48 +0330

    chore(release): release v5.4.1

--- src/Vajehbaz/Properties/AssemblyInfo.cs
+++ src/Vajehbaz/Properties/AssemblyInfo.cs
@@ -3,5 +3,5 @@
 [assembly: AssemblyTitle("Vajehbaz")]
 [assembly: AssemblyProduct("Vajehbaz")]
 [assembly: AssemblyDescription("A persian dictionary")]
-[assembly: AssemblyVersion("5.4.0.0")]
-[assembly: AssemblyFileVersion("5.4.0.0")]
+[assembly: AssemblyVersion("5.4.1.0")]
+[assembly: AssemblyFileVersion("5.4.1.0")]
