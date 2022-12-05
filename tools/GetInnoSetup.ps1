837f37c6aa3b609da56b24eab775111af449bee3
Alireza Rezaee <alireza@rezaee.org>
2022-12-05 13:57:00 +0330

    perf: prepend shebang to script files

--- tools/GetInnoSetup.ps1
+++ tools/GetInnoSetup.ps1
@@ -1,4 +1,7 @@
+#!/usr/bin/env pwsh
+
 # Download the latest stable InnoSetup
+# Human Readable Page: https://jrsoftware.org/isdl.php#stable
 $outdir = New-Item -ItemType Directory -Force -Path '.tmp'
 $outFilename = Join-Path $outdir 'innosetup.exe'
 Invoke-WebRequest 'https://jrsoftware.org/download.php/is.exe' -OutFile $outFilename
--- tools/GetNetRuntimes.ps1
+++ tools/GetNetRuntimes.ps1
@@ -1,3 +1,7 @@
+#!/usr/bin/env pwsh
+
+# Download the latest stable .Net 6.0 Desktop Runtimes
+# Human Readable Page: https://dotnet.microsoft.com/en-us/download/dotnet/6.0
 $runtimes = @{
 	'windowsdesktop-runtime-win-x86.exe' = 'https://aka.ms/dotnet/6.0/windowsdesktop-runtime-win-x86.exe';
 	'windowsdesktop-runtime-win-x64.exe' = 'https://aka.ms/dotnet/6.0/windowsdesktop-runtime-win-x64.exe'
